4325a0c9152b21c3693e40ffb9353a09c9c5e565
SoccyUwU <socc.pupet77943@gmail.com>
2026-03-29 01:04:43 -0400

    reformatted concrete drier

--- src/main/kotlin/org/sable/pointless_machine/casting/patterns/spells/ConcreteDrier.kt
+++ src/main/kotlin/org/sable/pointless_machine/casting/patterns/spells/ConcreteDrier.kt
@@ -14,11 +14,20 @@ import net.minecraft.util.math.BlockPos
 
 object ConcreteDrier : SpellAction{
     override val argc = 1
+    // I hate it here
+    val concreteBlocks = arrayOf(Blocks.WHITE_CONCRETE, Blocks.ORANGE_CONCRETE, Blocks.MAGENTA_CONCRETE,
+        Blocks.LIGHT_BLUE_CONCRETE, Blocks.YELLOW_CONCRETE, Blocks.LIME_CONCRETE, Blocks.PINK_CONCRETE,
+        Blocks.GRAY_CONCRETE, Blocks.LIGHT_GRAY_CONCRETE, Blocks.CYAN_CONCRETE, Blocks.PURPLE_CONCRETE,
+        Blocks.BLUE_CONCRETE, Blocks.BROWN_CONCRETE, Blocks.GREEN_CONCRETE, Blocks.RED_CONCRETE, Blocks.BLACK_CONCRETE)
+    val concretePowderBlocks = arrayOf(Blocks.WHITE_CONCRETE_POWDER, Blocks.ORANGE_CONCRETE_POWDER,
+        Blocks.MAGENTA_CONCRETE_POWDER, Blocks.LIGHT_BLUE_CONCRETE_POWDER, Blocks.YELLOW_CONCRETE_POWDER,
+        Blocks.LIME_CONCRETE_POWDER, Blocks.PINK_CONCRETE_POWDER, Blocks.GRAY_CONCRETE_POWDER,
+        Blocks.LIGHT_GRAY_CONCRETE_POWDER, Blocks.CYAN_CONCRETE_POWDER, Blocks.PURPLE_CONCRETE_POWDER,
+        Blocks.BLUE_CONCRETE_POWDER, Blocks.BROWN_CONCRETE_POWDER, Blocks.GREEN_CONCRETE_POWDER,
+        Blocks.RED_CONCRETE_POWDER, Blocks.BLACK_CONCRETE_POWDER)
     override fun execute(args : List<Iota>, env : CastingEnvironment) : SpellAction.Result {
         val target = args.getBlockPos(0, argc)
         env.assertVecInRange(target.toCenterPos())
-        // I hate it here
-        val concreteBlocks = arrayOf(Blocks.WHITE_CONCRETE, Blocks.ORANGE_CONCRETE, Blocks.MAGENTA_CONCRETE, Blocks.LIGHT_BLUE_CONCRETE, Blocks.YELLOW_CONCRETE, Blocks.LIME_CONCRETE, Blocks.PINK_CONCRETE, Blocks.GRAY_CONCRETE, Blocks.LIGHT_GRAY_CONCRETE, Blocks.CYAN_CONCRETE, Blocks.PURPLE_CONCRETE, Blocks.BLUE_CONCRETE, Blocks.BROWN_CONCRETE, Blocks.GREEN_CONCRETE, Blocks.RED_CONCRETE, Blocks.BLACK_CONCRETE)
         if(!concreteBlocks.contains(env.world.getBlockState(target).block)){
             throw MishapBadBlock(target, Text.of("concrete block"))
         }
@@ -32,8 +41,6 @@ object ConcreteDrier : SpellAction{
     private data class Spell(val target : BlockPos) : RenderedSpell{
         override fun cast(env: CastingEnvironment) {
             val block = env.world.getBlockState(target)
-            val concreteBlocks = arrayOf(Blocks.WHITE_CONCRETE, Blocks.ORANGE_CONCRETE, Blocks.MAGENTA_CONCRETE, Blocks.LIGHT_BLUE_CONCRETE, Blocks.YELLOW_CONCRETE, Blocks.LIME_CONCRETE, Blocks.PINK_CONCRETE, Blocks.GRAY_CONCRETE, Blocks.LIGHT_GRAY_CONCRETE, Blocks.CYAN_CONCRETE, Blocks.PURPLE_CONCRETE, Blocks.BLUE_CONCRETE, Blocks.BROWN_CONCRETE, Blocks.GREEN_CONCRETE, Blocks.RED_CONCRETE, Blocks.BLACK_CONCRETE)
-            val concretePowderBlocks = arrayOf(Blocks.WHITE_CONCRETE_POWDER, Blocks.ORANGE_CONCRETE_POWDER, Blocks.MAGENTA_CONCRETE_POWDER, Blocks.LIGHT_BLUE_CONCRETE_POWDER, Blocks.YELLOW_CONCRETE_POWDER, Blocks.LIME_CONCRETE_POWDER, Blocks.PINK_CONCRETE_POWDER, Blocks.GRAY_CONCRETE_POWDER, Blocks.LIGHT_GRAY_CONCRETE_POWDER, Blocks.CYAN_CONCRETE_POWDER, Blocks.PURPLE_CONCRETE_POWDER, Blocks.BLUE_CONCRETE_POWDER, Blocks.BROWN_CONCRETE_POWDER, Blocks.GREEN_CONCRETE_POWDER, Blocks.RED_CONCRETE_POWDER, Blocks.BLACK_CONCRETE_POWDER)
             env.world.setBlockState(target, concretePowderBlocks[concreteBlocks.indexOf(block.block)].defaultState)
         }
     }
